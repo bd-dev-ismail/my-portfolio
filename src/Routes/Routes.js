00f7a0218c73b14245fa8645a770172f4d6d45ab
bd-dev-ismail <ismailratul7@gmail.com>
2022-11-28 22:56:01 +0600

    iniital setup

---
+++ src/Routes/Routes.js
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import About from "../components/About/About";
+import Contact from "../components/Contact/Contact";
+import ErrorPage from "../components/ErrorPage/ErrorPage";
+import Home from "../components/Home/Home/Home";
+import Projects from "../components/Projects/Projects";
+import Main from "../layout/Main";
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main/>,
+        errorElement: <ErrorPage/>,
+        children: [
+            {
+                path: '/',
+                element: <Home/>
+            },
+            {
+                path: '/projects',
+                element: <Projects/>
+            },
+            {
+                path: '/about',
+                element: <About/>
+            },
+            {
+                path: '/contact',
+                element: <Contact/>
+            }
+        ]
+       
+    }
+])
---
+++ src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            Something Wrong!!1
+        </div>
+    );
+};
+
+export default ErrorPage;
