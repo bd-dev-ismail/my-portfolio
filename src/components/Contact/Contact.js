ec1bc495abb9cebdc2f51bbf388f94bb49fd7083
bd-dev-ismail <ismailratul7@gmail.com>
2022-12-10 19:34:06 +0600

    addd

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from "@emailjs/browser";
 import contact from '../../assets/contact.jpg';
+import { toast } from 'react-hot-toast';
 const Contact = () => {
+  //  const form = useRef();
+  // console.log(form);
+   const sendEmail = (e) => {
+     e.preventDefault();
+
+     emailjs
+       .sendForm(
+         process.env.REACT_APP_SERVICE_KEY,
+         process.env.REACT_APP_TEMPLATE_KEY,
+         e.target,
+         process.env.REACT_APP_PUBLIC_KEY
+       )
+       .then(
+         (result) => {
+           console.log(result);
+           console.log("mcg sent");
+           toast.success("Message Sent Successfully!");
+         },
+         (error) => {
+           console.log(error.text);
+         }
+       );
+   };
     return (
       <div>
         <div className="grid items-center max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-800 dark:text-gray-100">
@@ -16,14 +41,16 @@ const Contact = () => {
             <img src={contact} alt="" className="p-6 " />
           </div>
           <form
-            novalidate=""
+            // ref={form}
+            onSubmit={sendEmail}
             className="space-y-6 ng-untouched ng-pristine ng-valid"
           >
             <div>
-              <label for="name" className="text-sm">
+              <label htmlFor="name" className="text-sm">
                 Full name
               </label>
               <input
+                name="user_name"
                 id="name"
                 type="text"
                 placeholder=""
@@ -31,20 +58,22 @@ const Contact = () => {
               />
             </div>
             <div>
-              <label for="email" className="text-sm">
+              <label htmlFor="email" className="text-sm">
                 Email
               </label>
               <input
+                name="user_email"
                 id="email"
                 type="email"
                 className="w-full input input-bordered p-3 rounded dark:bg-gray-800"
               />
             </div>
             <div>
-              <label for="message" className="text-sm">
+              <label htmlFor="message" className="text-sm">
                 Message
               </label>
               <textarea
+                name="message"
                 id="message"
                 rows="3"
                 className="w-full textarea textarea-bordered  p-3 rounded dark:bg-gray-800"
